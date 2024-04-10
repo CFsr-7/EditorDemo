3106e88522c6410c59d2f8e82b3b68708bfce7f8
wzw <zhiwei_wang2@asdc.com.cn>
2024-04-10 16:23:19 +0800

    editor 上传文件基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <editor-view msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import editor from './components/Editor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    "editor-view":editor
   }
 }
 </script>
